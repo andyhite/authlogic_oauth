36c0f8165440d17d21ef7aa05157df526ddcf491
Andrew Hite <andrew@andrew-hite.com>
2010-08-15 20:10:09 -0500

    Moved inclusion of callback filter to a more appropriate location

--- lib/authlogic_oauth.rb
+++ lib/authlogic_oauth.rb
@@ -3,7 +3,21 @@ require File.dirname(__FILE__) + "/authlogic_oauth/oauth_process"
 require File.dirname(__FILE__) + "/authlogic_oauth/acts_as_authentic"
 require File.dirname(__FILE__) + "/authlogic_oauth/session"
 require File.dirname(__FILE__) + "/authlogic_oauth/helper"
+require File.dirname(__FILE__) + "/oauth_callback_filter"
 
 ActiveRecord::Base.send(:include, AuthlogicOauth::ActsAsAuthentic)
 Authlogic::Session::Base.send(:include, AuthlogicOauth::Session)
 ActionController::Base.helper AuthlogicOauth::Helper
+
+# Throw callback rack app into the middleware stack
+if defined?(ActionController::Metal)
+  module AuthlogicOauth
+    class Railtie < Rails::Railtie
+      initializer :load_oauth_callback_filter do |app|
+        app.config.middleware.use(OauthCallbackFilter) # Rails >= 3.0
+      end
+    end
+  end
+else
+  ActionController::Dispatcher.middleware.use(OauthCallbackFilter) # Rails < 3.0
+end
--- rails/init.rb
+++ rails/init.rb
@@ -1,15 +1,1 @@
 require "authlogic_oauth"
-require "oauth_callback_filter"
-
-# Throw callback rack app into the middleware stack
-if defined?(ActionController::Metal)
-  module AuthlogicOauth
-    class Railtie < Rails::Railtie
-      initializer :load_oauth_callback_filter do |app|
-        app.config.middleware.use(OauthCallbackFilter) # Rails >= 3.0
-      end
-    end
-  end
-else
-  ActionController::Dispatcher.middleware.use(OauthCallbackFilter) # Rails < 3.0
-end
